10eebe3ffc033020e888443a92ddfe8e2f9e7827
Injazzz <kamal.akbarzy@gmail.com>
2025-06-04 04:31:35 +0700

    fix and add feature

--- src/app/guest/login/page.tsx
+++ src/app/guest/login/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function GuestLoginPage() {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <div className='w-full p-8 space-y-6 rounded-lg shadow-lg'>
+      <div className='w-full p-8 space-y-6 rounded-lg'>
         <div className='w-full max-w-sm mx-auto'>
           {" "}
           <GuestLoginForm />
